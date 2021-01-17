4af9de24bb29c4362d8f4c348e0607698923890e
lichenjie <2502912097@qq.com>
2021-01-17 20:11:38 +0800

    属性查询完善

--- shop_product/src/main/java/com/fh/model/po/Attribute.java
+++ shop_product/src/main/java/com/fh/model/po/Attribute.java
@@ -32,6 +32,16 @@ public class Attribute {
 
     private String   author; //操作人
 
+    private String typeName; // 分类名称
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
     public Integer getId() {
         return id;
     }
